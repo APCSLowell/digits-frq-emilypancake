0842c59db54389762065dd65e0c274a6f18e5142
Emily Pan <pancakerula@gmail.com>
2025-04-11 23:49:34 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,12 +7,24 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	    	digitList = new ArrayList <Integer>();
+		while(num != 0){
+			digitList.add(0, num%10);
+		}
+		
+		    
+	    }
+		
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		for(int i = 0;i<digitList.size() - 1;i++){
+			if(digitList.get(i+1) <= digitList.get(i)){
+				return false;
+			}
+		}
+		return true;
 
 	}
 	
